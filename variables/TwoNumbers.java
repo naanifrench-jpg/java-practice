fc6aa5b524381f4c9a29a81404638be90af43e6e
nanteza <naani.french@gmail.com>
2026-07-25 19:23:13 -0400

    Add files via upload

---
+++ variables/TwoNumbers.java
@@ -0,0 +1,8 @@
+public class TwoNumbers {
+    public static void main (String[] args) {
+        // write a program that prints the sum of two numbers.
+        int number1 = 74;
+        int number2 = 36;
+        System.out.print(number1 + number2);
+    }
+}
